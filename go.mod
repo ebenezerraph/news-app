73b2c703bfb9099ef27e74e01c1d2688df21bb86
Ebenezer Raphael <ebenezerraphael0@gmail.com>
2024-04-21 18:07:26 +0100

    Render

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/ebenezerraph/news-app
 go 1.22.2
 
 require github.com/joho/godotenv v1.5.1
+
+require github.com/freshman-tech/news-demo v0.0.0-20201226085618-d28e34153fe9
--- main.go
+++ main.go
@@ -1,15 +1,89 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"html/template"
 	"log"
+	"math"
 	"net/http"
+	"net/url"
 	"os"
+	"strconv"
+	"time"
 	
+	"github.com/freshman-tech/news-demo/news"
 	"github.com/joho/godotenv"
 )
 
+var tpl = template.Must(template.ParseFiles("index.html"))
+
+type Search struct {
+	Query      string
+	NextPage   int
+	TotalPages int
+	Results    *news.Results
+}
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Hello World!</h1>"))
+	buf := &bytes.Buffer{}
+	err := tpl.Execute(buf, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	buf.WriteTo(w)
+}
+
+func searchHandler (newsapi *news.Client) http.HandlerFunc {
+	return func (w http.ResponseWriter, r *http.Request) {
+		u, err := url.Parse(r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		params := u.Query()
+		searchQuery := params	.Get("q")
+		page := params.Get("page")
+		if page == "" {
+			page = "1"
+		}
+
+		results, err := newsapi.FetchEverything(searchQuery, page)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		nextPage, err := strconv.Atoi(page)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		search := &Search{
+			Query:      searchQuery,
+			NextPage:   nextPage,
+			TotalPages: int(math.Ceil(float64(results.TotalResults) / float64(newsapi.PageSize))),
+			Results:    results,
+		}
+
+		buf := &bytes.Buffer{}
+		err = tpl.Execute(buf, search)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		buf.WriteTo(w)
+
+		fmt.Printf("%+v", results)
+
+		fmt.Println("Search Query is: ", searchQuery)
+		fmt.Println("Page is: ", page)
+	}
 }
 
 func main() {
@@ -18,14 +92,24 @@ func main() {
 		log.Println("Error loading .env file")
 	}
 
-
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
 	}
 
-	mux := http.NewServeMux()
+	apiKey := os.Getenv("NEWS_API_KEY")
+	if apiKey == "" {
+		log.Fatal("Env: apiKey must be set")
+	}
 
+	myClient := &http.Client{Timeout: 10 * time.Second}
+	newsapi := news.NewClient(myClient, apiKey, 20)
+
+	fs := http.FileServer(http.Dir("assets"))
+
+	mux := http.NewServeMux()
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
 	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/search", searchHandler(newsapi))
 	http.ListenAndServe(":"+port, mux)
 }
