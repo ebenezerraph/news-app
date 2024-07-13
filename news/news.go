d876ff3e78a4bfd357052c57ffbd08636f83c9e9
Ebenezer Raphael <ebenezerraphael0@gmail.com>
2024-07-13 23:20:34 +0100

    feat: Improve user experience and search functionality
    Improved the user interface for a better visual appeal and usability, including refined search input design, error message handling, and visual enhancements for the results display. Updated the search functionality to return more relevant results and handle edge cases.

--- news/news.go
+++ news/news.go
@@ -15,7 +15,6 @@ type Client struct {
 	PageSize int
 }
 
-
 func (c *Client) FetchEverything(query, page string) (*Results, error) {
 	endpoint := fmt.Sprintf("https://newsapi.org/v2/everything?q=%s&pageSize=%d&page=%s&apiKey=%s&sortBy=publishedAt&language=en", url.QueryEscape(query), c.PageSize, page, c.key)
 	resp, err := c.http.Get(endpoint)
