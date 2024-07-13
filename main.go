f393e89ef6287143ed9be2b98a120b622787ff10
Ebenezer Raphael <ebenezerraphael0@gmail.com>
2024-07-14 00:32:20 +0100

    Update

--- main.go
+++ main.go
@@ -271,7 +271,6 @@ func main() {
 
 	apiKey := os.Getenv("NEWS_API_KEY")
 	if apiKey == "" {
-		apiKey = "115c54bbd699469281a40befde1a5071"
 		log.Fatal("NEWS_API_KEY must be set in .env file or system environment")
 	}
 
